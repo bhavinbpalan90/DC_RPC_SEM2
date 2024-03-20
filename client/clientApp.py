5237d4b6035588c88be10b5d6b35bc2589ba362d
bhavinbpalan90 <118091056+bhavinbpalan90@users.noreply.github.com>
2024-03-19 21:49:30 -0400

    Client Code

---
+++ client/clientApp.py
@@ -0,0 +1,47 @@
+from rpcClient import RPCClient
+import os.path
+import streamlit as st
+import pathlib
+
+st.set_page_config(layout="wide")
+col1, col2,col3 = st.columns([1,4,2])
+with col1:
+    input_ipaddress = st.text_input('Server IP Address','192.168.12.130')
+    input_port = st.text_input('Port No',8080)
+
+with col2:
+    input_platform = st.selectbox(
+        'Please select the Platform from which you want the content ?',
+        ('Netflix', 'Prime', 'HBO'))
+
+    input_fileName = st.text_input('File Name', '')
+
+    if st.button('Get File') and input_fileName != '':
+        server = RPCClient(str(input_ipaddress), int(input_port))
+        server.connect()
+        try:
+            Platform=str(input_platform)
+            fileName = str(input_fileName)
+            if not os.path.isdir('Contents'):
+                os.mkdir('Contents')
+            directory = 'Contents/' + str(Platform) + '/'
+            if not os.path.isdir(directory):
+                os.mkdir(directory)
+                #st.write('directory created')
+            ##file_path  = os.path.join(directory, fileName)
+            file_path = str(os.getcwd()) + str('\Contents\\') + str(Platform) + str('\\') + str(fileName)
+            #st.write(file_path)
+            handle=open(file_path,"w")
+            content = str(server.movefile(str(Platform),str(fileName)))
+            handle.write(content)
+            st.write('File Downloaded and available at ' + str(file_path))
+            handle.close()
+        except:
+            st.write ('Download failed')
+        server.disconnect()
+
+with col3:
+    st.write('All files available locally are as below: ')
+    Content = pathlib.Path('Contents\\')
+    for files in Content.rglob('*.*'):
+        st.write(files)
